879925bf146e2a853c28bcc049e9541722d3c663
Chowdhury-Anik <chowdhuryanik1998@gmail.com>
2025-11-30 01:15:53 +0600

    Update __init__.py
    add the missing ' )' in str(files(__name__).joinpath(...))

--- face_recognition_models/__init__.py
+++ face_recognition_models/__init__.py
@@ -7,14 +7,14 @@ __version__ = '0.1.0'
 from importlib.resources import files
 
 def pose_predictor_model_location():
-    return str(files(__name__).joinpath("models/shape_predictor_68_face_landmarks.dat")
+    return str(files(__name__).joinpath("models/shape_predictor_68_face_landmarks.dat"))
 
 def pose_predictor_five_point_model_location():
-    return str(files(__name__).joinpath("models/shape_predictor_5_face_landmarks.dat")
+    return str(files(__name__).joinpath("models/shape_predictor_5_face_landmarks.dat"))
 
 def face_recognition_model_location():
-    return str(files(__name__).joinpath("models/dlib_face_recognition_resnet_model_v1.dat")
+    return str(files(__name__).joinpath("models/dlib_face_recognition_resnet_model_v1.dat"))
 
 def cnn_face_detector_model_location():
-    return str(files(__name__).joinpath("models/mmod_human_face_detector.dat")
+    return str(files(__name__).joinpath("models/mmod_human_face_detector.dat"))
 
